aac5f3f7af8381b2ec1a2d2a30c3845585c7304b
FSorrentinoC <sorrentino@live.com>
2020-07-16 14:20:28 -0300

    insert implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,10 +7,13 @@ import model.entites.Seller;
 
 public interface SellerDao {
 
-		void insert(Seller obj);
-		void update(Seller obj);
-		void deleteById(Integer id);
-		Seller findById(Integer id);
-		List<Seller> findAll();
-		List<Seller> findByDepartment(Department department);
-	}
+		
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+	List<Seller> findByDepartment(Department department);
+					
+		}
+
